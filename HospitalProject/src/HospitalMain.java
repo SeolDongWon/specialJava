4c96ab5bf51d705455d4cdc86826a9786f3a5965
SeolDongWon <seol.east@gmail.com>
2023-10-04 13:21:08 +0900

    [삭제 BookMarketProject, HospitalProject]

--- HospitalProject/src/HospitalMain.java
+++
@@ -1,9 +0,0 @@
-import com.controller.HospitalInfoController;
-
-public class HospitalMain {
-	public static void main(String[] args) {
-		HospitalInfoController controller = new HospitalInfoController();
-		controller.inputData();
-		controller.printHospitalData();
-	}
-}
